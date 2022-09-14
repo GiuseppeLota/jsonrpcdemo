33a2e4c3f77ce8795d62eadd3b3cfecf2a2174a9
giuseppe.lota <giuseppe.lota@altroconsumo.it>
2022-09-14 20:17:20 +0200

    refactor webclient instantiation

--- src/main/java/com/martinbohm/demo/ApplicationConfig.java
+++ src/main/java/com/martinbohm/demo/ApplicationConfig.java
@@ -2,7 +2,16 @@ package com.martinbohm.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ClientCodecConfigurer;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.googlecode.jsonrpc4j.spring.AutoJsonRpcServiceImplExporter;
 
 @Configuration
@@ -11,9 +20,25 @@ public class ApplicationConfig {
     @Bean
     public static AutoJsonRpcServiceImplExporter autoJsonRpcServiceImplExporter() {
         AutoJsonRpcServiceImplExporter exp = new AutoJsonRpcServiceImplExporter();
-        //in here you can provide custom HTTP status code providers etc. eg:
-        //exp.setHttpStatusCodeProvider();
-        //exp.setErrorResolver();
+        // in here you can provide custom HTTP status code providers etc. eg:
+        // exp.setHttpStatusCodeProvider();
+        // exp.setErrorResolver();
         return exp;
+    }
+
+    @Bean
+    public WebClient getWebClient() {
+        return WebClient.builder()
+                .baseUrl("https://api.adviceslip.com")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .exchangeStrategies(ExchangeStrategies.builder().codecs(this::acceptedCodecs).build())
+                .build();
+    }
+
+    private void acceptedCodecs(ClientCodecConfigurer clientCodecConfigurer) {
+        clientCodecConfigurer.customCodecs()
+                .registerWithDefaultConfig(new Jackson2JsonDecoder(new ObjectMapper(), MimeTypeUtils.TEXT_HTML));
+        clientCodecConfigurer.customCodecs()
+                .registerWithDefaultConfig(new Jackson2JsonEncoder(new ObjectMapper(), MimeTypeUtils.TEXT_HTML));
     }
 }
--- src/main/java/com/martinbohm/demo/externalProviders/impl/adviceSlipProvider.java
+++ src/main/java/com/martinbohm/demo/externalProviders/impl/adviceSlipProvider.java
@@ -1,40 +1,28 @@
 package com.martinbohm.demo.externalProviders.impl;
 
-import org.springframework.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.http.codec.ClientCodecConfigurer;
-import org.springframework.http.codec.json.Jackson2JsonDecoder;
-import org.springframework.http.codec.json.Jackson2JsonEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.util.MimeTypeUtils;
-import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.martinbohm.demo.dto.AdviceResponse;
 import com.martinbohm.demo.externalProviders.AdviceProvider;
 
 @Service
 public class adviceSlipProvider implements AdviceProvider {
 
+    @Autowired
+    WebClient webClient;
+
     public AdviceResponse findAll() {
-        return WebClient.builder()
-                .baseUrl("https://api.adviceslip.com")
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .exchangeStrategies(ExchangeStrategies.builder().codecs(this::acceptedCodecs).build())
-                .build()
+        return webClient
                 .get()
                 .uri("/advice/search/spiders")
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .toEntity(AdviceResponse.class)
                 .block()
-                .getBody(); 
+                .getBody();
     }
-
-    private void acceptedCodecs(ClientCodecConfigurer clientCodecConfigurer) {
-        clientCodecConfigurer.customCodecs().registerWithDefaultConfig(new Jackson2JsonDecoder(new ObjectMapper(), MimeTypeUtils.TEXT_HTML));
-        clientCodecConfigurer.customCodecs().registerWithDefaultConfig(new Jackson2JsonEncoder(new ObjectMapper(), MimeTypeUtils.TEXT_HTML));
-      }
 
 }
